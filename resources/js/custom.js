6a224c0c040392a97864dce6d7e5b795f44f31df
anupant01 <pantaanupa@gmail.com>
2019-06-27 11:16:30 +0545

    user and craft js created with design

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,13 +1,14 @@
 $(document).ready(
 
-	function ()
-	{
-		//form ko value
-		$('#registrationForm').submit(function(event)
-			{
-				event.preventDefault();
+	function () {
+		//formdata upload
+		//for registration
+		$('#registerForm').submit(function (event) {
+			event.preventDefault();
+			console.log('register button clicked');
 
-				var regFormData ={
+			var formdata = new FormData();
+			var registrationData = {
 
 				firstName: $('#firstName').val(),
 				lastName: $('#lastName').val(),
@@ -15,36 +16,90 @@ $(document).ready(
 				address: $('#address').val(),
 				username: $('#username').val(),
 				password: $('#password').val(),
+				images: $('#images')[0].files[0],
+				usertype: 'User'
+
 			}
 
-			
+			console.log(registrationData);
+			// images: $('#images')[0].files[0]
+
+			for (key in registrationData) {
+				// console.log(registrationData[key]);	
+				formdata.append(key, registrationData[key]);
+			}
 
 
-$.ajax({
-//v1:version, users:route in the backend
-//api name should be used in Noun
-url: 'http://localhost:9000/v1/users',
-method: 'POST',
-contentType: 'application/json',
-//javastring lai string ms change garcha
-data: JSON.stringify(regFormData),
+			$.ajax({
+				url: 'http://localhost:7000/v1/register',
+				method: 'POST',
+				processData: false,
+				contentType: false,
+				data: formdata,
+				dataType: 'json',
+	
+				success: function (result, status) {
+					// console.log(result.message);
+					console.log(result);
+					console.log(status);
+					console.log('asdasdasd');
+				},
+				error: function (jqXHR, status) {
+					console.log(jqXHR)
+					console.log('qwert')
+	
+				}
+			});
 
 
-success: function(result,status){
-console.log(result);
-console.log(status);
-},
 
-error:function(jqXHR,status){
-console.log(jqXHR.responseJSON.message)
-}
-
-
-})
+		})
 
 
 
-})
+		// for login
+		$('#loginForm').submit(function (event) {
+			event.preventDefault();
+			console.log('login button clicked');
+			const loginData = {
+				username: $('#username').val(),
+				password: $('#password').val()
+			}
+
+			$.ajax({
+				url: 'http://localhost:7000/v1/sign',
+				method: 'POST',
+				contentType: 'application/json',
+				data: JSON.stringify(loginData),
+				dataType: 'json',
+				success: function (result, status) {
+
+					window.localStorage.setItem('token', result.token);
+					window.localStorage.setItem('id', result.id);
+					window.localStorage.setItem('username', result.username);
 
 
-})
+					// console.log(result.result.usertype)
+					if (result.result.usertype == "User") {
+						window.location.href = 'index.html'
+
+					}
+					else {
+						window.location.href = 'adminDashboard.html'
+						// 		console.log('admin');
+
+
+
+					}
+				},
+
+				error: function (jqXHR, status) {
+					console.log(jqXHR);
+					//$("#message").html(jqXHR.responseJSON.message);
+				}
+			})
+		})
+
+
+
+	})
